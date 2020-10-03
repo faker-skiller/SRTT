51c11d7b234b9387cdf3042c7fa10e7c560f3ddb
Adam Malkovitch <34224353+RandomMcSomethin@users.noreply.github.com>
2020-10-02 21:31:35 -0500

    Candy corn and Halloween Hills

--- Stick Ranger Modded.js
+++ Stick Ranger Modded.js
@@ -300,7 +300,7 @@ var Item_Catalogue = Array(558); // array of item arrays original name: u
 Item_Catalogue[0] =  [""       ,0,0  ,0 ,0 ,Class_Stickman,0         ,0,0,0,0 ,0 ,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 Item_Catalogue[59] = ["NG"     ,0,0  ,13,12,Class_Compo   ,0xFF888888,0,0,0,0 ,0 ,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 Item_Catalogue[1] =  ["gold"   ,0,0  ,1 ,0 ,Class_Pickup  ,0xFFFFFFFF,1,0,0,0 ,0 ,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-Item_Catalogue[2] =  ["onigiri",0,0  ,2 ,0 ,Class_Pickup  ,0xFFFFFFFF,1,0,0,0 ,0 ,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+Item_Catalogue[2] =  ["candy corn",0,0  ,2 ,0 ,Class_Pickup  ,0xFFFFFFFF,1,0,0,0 ,0 ,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 //   gloves   [   ] = [0                ,1,2   ,3,4 ,5,6         ,7,8   ,9,10,11 ,12,13,14,15,16,7,18,19       ,20,21,22,23,24,25,26 ,27 ,28,9,30,31,2,33,4,35,36,37,8,39,40 ,41];
 Item_Catalogue[3] =   ["glove"          ,0,100 ,3,2 ,1,0xFF6666FF,1,0   ,0,1 ,3  ,0 ,0 ,15,20,15,0,0 ,0         ,1,0 ,0 ,0 ,0 ,0 ,0  ,0  ,0 ,0,0  ,0,0,0 ,0,0 ,0  ,0];// ,   ,
@@ -10707,7 +10707,7 @@ var Stage_Spawns = [
         [0,0  ,Ground,0,0,Ground,0,0,Ground,0,0,Ground,0,0],
         [0,100,Ground,0]
     ],
-    [   //1 Opening Street
+    [   //1 Halloween Hills
         [0,1  ,Ground     ,0,5 ,Ground_Middle,1,1],
         //[0,100,Ground_Left,4,1], // training ground
         [0,1  ,Ground     ,0,6 ,Ground_Middle,1,3],
